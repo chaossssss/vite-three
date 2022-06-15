3ab11e4f15663a6e27dddec8565897e847a3543b
LAPTOP-UAONIT64\admin <1033729276@qq.com>
2022-06-15 10:16:33 +0800

    fix:无聊

--- src/components/Map.vue
+++ src/components/Map.vue
@@ -18,9 +18,45 @@ const clock = new THREE.Clock()
 var time = { value: 0 }
 var startTime = { value: 0 }
 var startLength = { value: 2 }
+
+
+let ratio = {
+  value: 0
+}
+
+// 雷达扫描的相关配置数据
+const radarData = [{
+  position: {
+    x: -100,
+    y: 0,
+    z: 0
+  },
+  radius: 80,
+  color: '#ff0062',
+  opacity: 0.5,
+  angle: Math.PI * 2,
+  speed: 2
+}, {
+  position: {
+    x: 100,
+    y: 0,
+    z: 10
+  },
+  radius: 50,
+  color: '#f000f2',
+  opacity: 0.5,
+  angle: Math.PI,
+  speed: 2
+}];
+
+
+
+
+
 onMounted(() => {
   init()
   createRadar()
+  // initRadar(radarData)
   initStats()
   initMap()
   animate()
@@ -168,11 +204,11 @@ function createRadar() {
       y: 20,
       z: 0
     },
-    radius: 240,
+    radius: 2,
     color: '#f005f0',
     opacity: 0.5,
     speed: 300,
-    followWidth: 220,
+    followWidth: 20,
   }
 
   // 创建几何体
@@ -242,7 +278,7 @@ function createRadar() {
       `
     const fragementColor = `
         // length内置函数，取向量的长度
-        if(length(vPosition) == 0.0 || length(vPosition) > uRadius-2.0){
+        if(length(vPosition) == 0.0 || length(vPosition) > uRadius-0.1){  // 最后这个外框
           gl_FragColor = vec4( outgoingLight, diffuseColor.a );
         } else {
           float angle = calcAngle(vPosition);
@@ -265,11 +301,124 @@ function updateData() {
   const dt = clock.getDelta()
   time.value += dt
   startTime.value += dt
+  ratio.value += dt
   if (startTime.value > startLength.value) {
     startTime.value = startLength.value
   }
 }
 
+function initRadar(options) {
+
+
+  const vertexShader = `
+            precision mediump float;
+            precision highp int;
+
+            varying vec2 vPosition;
+
+            void main () {
+                vPosition = vec2(position.x, position.y);
+                gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }`;
+  const fragmentShader = `
+            precision mediump float;
+            precision highp int;
+
+            uniform float uTime;
+            uniform float u_radius;
+            uniform float u_speed;
+            uniform float u_opacity;
+            uniform float u_width;
+            uniform vec3 u_color;
+
+            varying vec2 vPosition;
+            #define PI 3.14159265359
+
+            void main () {
+                // 计算当前扫描旋转的弧度值总数
+                float currentRadius = u_speed * uTime;
+
+                // 计算当前像素点与原点连线和x轴构成的夹角的弧度值
+                // atan 接受两个参数（y,x）时 等同于 atan2,返回的是atan(y/x)；
+                // 但比后者更稳定，返回值区间[-PI, PI]
+                float angle = atan(vPosition.y, vPosition.x) + PI;
+
+                // 计算当前像素低旋转后的弧度值，值固定在[0, PI * 2]之间
+                float angleT = mod(currentRadius + angle, PI * 2.0);
+
+                // 计算当前位置距离中心点距离
+                float dist = distance(vec2(0.0, 0.0), vPosition);
+                
+                float tempOpacity = 0.0;
+
+                // 设置雷达外层圆环的宽度
+                float circleWidth = 5.0;
+                // 如果当前点在外层圆环上， 设置一个透明度
+                if (dist < u_radius && dist > u_radius - circleWidth) {
+                    // 做一个虚化渐变效果
+                    float pct = smoothstep(u_radius - circleWidth, u_radius, dist);
+                    tempOpacity = sin(pct * PI);
+                }
+
+                // 设置雷达扫描圈的效果 (-5.0是给外层圆环和内层圆之间设置一点空白间距)
+                if (dist < (u_radius - 5.0)) {
+                    tempOpacity = 1.0 - angleT / u_width;
+                }
+
+
+
+                gl_FragColor = vec4(u_color, u_opacity * tempOpacity);
+        }
+        `;
+
+
+
+
+
+  const {
+    position,
+    radius,
+    color,
+    opacity,
+    speed,
+    angle,
+  } = options;
+
+  const size = radius * 2;
+
+  let plane = new THREE.PlaneGeometry(size, size);
+  let material_1 = new THREE.ShaderMaterial({
+    transparent: true,
+    depthWrite: false,
+    side: THREE.DoubleSide,
+    uniforms: {
+      uTime: ratio,
+      u_radius: {
+        value: radius
+      },
+      u_speed: {
+        value: speed
+      },
+      u_opacity: {
+        value: opacity
+      },
+      u_width: {
+        value: angle
+      },
+      u_color: {
+        value: new THREE.Color(color)
+      },
+
+    },
+    vertexShader,
+    fragmentShader
+  })
+  let planeMesh = new THREE.Mesh(plane, material_1);
+  scene.add(planeMesh);
+
+  planeMesh.rotation.x = -0.5 * Math.PI
+  planeMesh.position.copy(position);
+}
 
 function animate() {
   requestAnimationFrame(animate)
