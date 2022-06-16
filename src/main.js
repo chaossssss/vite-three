078ffaf52421e3c74909d7491c9375383ef80edf
LAPTOP-UAONIT64\admin <1033729276@qq.com>
2022-06-16 14:29:29 +0800

    feat:ele

--- src/main.js
+++ src/main.js
@@ -2,7 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import '@/assets/reset.css'
 import router from '@/router/index'
-
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 
 const app = createApp(App)
 app.use(router)
